64ecaddbb17d917e4160884c9f90508e44421759
Paco Moreno <pc.moreno2099@gmail.com>
2018-06-28 10:26:04 -0500

    Search DataSource in Context before loading from config file

--- src/main/java/mx/nic/lab/rpki/sqlite/impl/database/DatabaseSession.java
+++ src/main/java/mx/nic/lab/rpki/sqlite/impl/database/DatabaseSession.java
@@ -8,6 +8,9 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.sqlite.SQLiteDataSource;
@@ -31,7 +34,66 @@ public class DatabaseSession {
 	 * @throws InitializationException
 	 */
 	public static void initConnection(Properties config) throws InitializationException {
+		/*
+		 * At compile time, the application does not know the data access
+		 * implementation, and the data access implementation does not know the
+		 * application.
+		 * 
+		 * So we don't know for sure where the data source can be found, and the
+		 * application cannot tell us. There is no interface for the application to
+		 * provide us with a data source, because the data source is OUR problem. From
+		 * the app's perspective, it might not even exist.
+		 * 
+		 * So what we're going to do is probe the candidate locations and stick with
+		 * what works.
+		 */
+		dataSource = findDataSource(config);
+		if (dataSource != null) {
+			logger.info("Data source found.");
+			return;
+		}
+		logger.info("I could not find the API data source in the context. "
+				+ "This won't be a problem if I can find it in the configuration. Attempting that now... ");
 		dataSource = loadDataSourceFromProperties(config);
+	}
+
+	private static DataSource findDataSource(Properties config) {
+		Context context;
+		try {
+			context = new InitialContext();
+		} catch (NamingException e) {
+			logger.log(Level.INFO, "I could not instance an initial context. "
+					+ "I will not be able to find the data source by JDNI name.", e);
+			return null;
+		}
+
+		String jdniName = config.getProperty("db_resource_name");
+		if (jdniName != null) {
+			return findDataSource(context, jdniName);
+		}
+
+		// Try the default string.
+		// In some server containers (such as Wildfly), the default string is
+		// "java:/comp/env/jdbc/rpki-api".
+		// In other server containers (such as Payara), the string is
+		// "java:comp/env/jdbc/rpki-api".
+		// In other server containers (such as Tomcat), it doesn't matter.
+		DataSource result = findDataSource(context, "java:/comp/env/jdbc/rpki-api");
+		if (result != null) {
+			return result;
+		}
+
+		return findDataSource(context, "java:comp/env/jdbc/rpki-api");
+	}
+
+	private static DataSource findDataSource(Context context, String jdniName) {
+		logger.info("Attempting to find data source '" + jdniName + "'...");
+		try {
+			return (DataSource) context.lookup(jdniName);
+		} catch (NamingException e) {
+			logger.info("Data source not found. Attempting something else...");
+			return null;
+		}
 	}
 
 	private static DataSource loadDataSourceFromProperties(Properties config) throws InitializationException {
