a30ce1846db3e8440e286b383b61224b00e517ee
Paco Moreno <pc.moreno2099@gmail.com>
2018-07-27 17:08:36 -0500

    Add "slurm" servlet to retrieve the whole SLURM

---
+++ src/main/java/mx/nic/lab/rpki/api/result/slurm/SlurmResult.java
@@ -0,0 +1,90 @@
+package mx.nic.lab.rpki.api.result.slurm;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonStructure;
+
+import mx.nic.lab.rpki.api.result.ApiSingleResult;
+import mx.nic.lab.rpki.db.pojo.Slurm;
+import mx.nic.lab.rpki.db.pojo.SlurmBgpsec;
+import mx.nic.lab.rpki.db.pojo.SlurmPrefix;
+
+/**
+ * Result that represents the complete SLURM
+ *
+ */
+public class SlurmResult extends ApiSingleResult<Slurm> {
+
+	public SlurmResult(Slurm slurm) {
+		super();
+		setApiObject(slurm);
+	}
+
+	@Override
+	public JsonStructure toJsonStructure() {
+		Slurm slurm = getApiObject();
+		if (slurm == null) {
+			return JsonObject.EMPTY_JSON_OBJECT;
+		}
+		// Response conforming the original SLURM as indicated at
+		// draft-ietf-sidr-slurm-08
+		JsonObjectBuilder slurmBuilder = Json.createObjectBuilder();
+		JsonObjectBuilder validationOutputFilters = Json.createObjectBuilder();
+		JsonObjectBuilder locallyAddedAssertions = Json.createObjectBuilder();
+		JsonArrayBuilder prefixFilters = Json.createArrayBuilder();
+		JsonArrayBuilder prefixAssertions = Json.createArrayBuilder();
+		JsonArrayBuilder bgpsecFilters = Json.createArrayBuilder();
+		JsonArrayBuilder bgpsecAssertions = Json.createArrayBuilder();
+		slurm.getPrefixes().forEach(prefix -> {
+			if (prefix.getType() == SlurmPrefix.TYPE_FILTER) {
+				prefixFilters.add(getFormattedPrefix(prefix));
+			} else if (prefix.getType() == SlurmPrefix.TYPE_ASSERTION) {
+				prefixAssertions.add(getFormattedPrefix(prefix));
+
+			}
+		});
+		slurm.getBgpsecs().forEach(bgpsec -> {
+			if (bgpsec.getType() == SlurmBgpsec.TYPE_FILTER) {
+				prefixFilters.add(getFormattedBgpsec(bgpsec));
+			} else if (bgpsec.getType() == SlurmBgpsec.TYPE_ASSERTION) {
+				prefixAssertions.add(getFormattedBgpsec(bgpsec));
+
+			}
+		});
+
+		validationOutputFilters.add("prefixFilters", prefixFilters);
+		validationOutputFilters.add("bgpsecFilters", bgpsecFilters);
+
+		locallyAddedAssertions.add("prefixAssertions", prefixAssertions);
+		locallyAddedAssertions.add("bgpsecAssertions", bgpsecAssertions);
+
+		slurmBuilder.add("slurmVersion", 1);
+		slurmBuilder.add("validationOutputFilters", validationOutputFilters);
+		slurmBuilder.add("locallyAddedAssertions", locallyAddedAssertions);
+
+		return slurmBuilder.build();
+	}
+
+	private JsonObject getFormattedPrefix(SlurmPrefix prefix) {
+		JsonObjectBuilder builder = Json.createObjectBuilder();
+		if (prefix.getPrefixText() != null && !prefix.getPrefixText().isEmpty()) {
+			String value = prefix.getPrefixText().concat("/").concat(prefix.getPrefixLength().toString());
+			addKeyValueToBuilder(builder, "prefix", value, false);
+		}
+		addKeyValueToBuilder(builder, "asn", prefix.getAsn(), false);
+		addKeyValueToBuilder(builder, "maxPrefixLength", prefix.getPrefixMaxLength(), false);
+		addKeyValueToBuilder(builder, "comment", prefix.getComment(), false);
+		return builder.build();
+	}
+
+	private JsonObject getFormattedBgpsec(SlurmBgpsec bgpsec) {
+		JsonObjectBuilder builder = Json.createObjectBuilder();
+		addKeyValueToBuilder(builder, "asn", bgpsec.getAsn(), false);
+		addKeyValueToBuilder(builder, "SKI", bgpsec.getSki(), false);
+		addKeyValueToBuilder(builder, "publicKey", bgpsec.getPublicKey(), false);
+		addKeyValueToBuilder(builder, "comment", bgpsec.getComment(), false);
+		return builder.build();
+	}
+}
---
+++ src/main/java/mx/nic/lab/rpki/api/servlet/slurm/SlurmServlet.java
@@ -0,0 +1,53 @@
+package mx.nic.lab.rpki.api.servlet.slurm;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import mx.nic.lab.rpki.api.result.ApiResult;
+import mx.nic.lab.rpki.api.result.slurm.SlurmResult;
+import mx.nic.lab.rpki.api.servlet.DataAccessServlet;
+import mx.nic.lab.rpki.api.servlet.RequestMethod;
+import mx.nic.lab.rpki.db.exception.ApiDataAccessException;
+import mx.nic.lab.rpki.db.exception.http.HttpException;
+import mx.nic.lab.rpki.db.pojo.Slurm;
+import mx.nic.lab.rpki.db.service.DataAccessService;
+import mx.nic.lab.rpki.db.spi.SlurmDAO;
+
+/**
+ * Servlet to provide the complete SLURM
+ *
+ */
+@WebServlet(name = "slurm", value = { "/slurm" })
+public class SlurmServlet extends DataAccessServlet<SlurmDAO> {
+
+	/**
+	 * Serial version ID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected ApiResult doApiDaRequest(RequestMethod requestMethod, HttpServletRequest request, SlurmDAO dao)
+			throws HttpException, ApiDataAccessException {
+		Slurm slurm = dao.getAll();
+		return new SlurmResult(slurm);
+	}
+
+	@Override
+	protected String getServedObjectName() {
+		return "slurm";
+	}
+
+	@Override
+	protected List<RequestMethod> getSupportedRequestMethods() {
+		return Arrays.asList(RequestMethod.GET);
+	}
+
+	@Override
+	protected SlurmDAO initAccessDAO() throws ApiDataAccessException {
+		return DataAccessService.getSlurmDAO();
+	}
+
+}
